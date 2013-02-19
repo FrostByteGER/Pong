669b7be0ec61538408f0cfe0da820a65449f1d6c
Kevin Kuegler <Kevin2595@live.de>
2013-02-19 21:26:04 +0100

    [ADDED] Network package and basic serverclass for multiplayer and lan

---
+++ Java - Pong/src/de/frostbyteger/pong/engine/network/PongServer.java
@@ -0,0 +1,13 @@
+package de.frostbyteger.pong.engine.network;
+
+public class PongServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
